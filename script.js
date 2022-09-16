7ab5357ad6f42041994643942f3cc302998cef0c
MhamadR <mhamadraslan@gmail.com>
2022-09-16 21:22:55 +0300

    Added event listener and flipCard()

---
+++ script.js
@@ -0,0 +1,23 @@
+const cards = document.querySelectorAll(".card");
+const result = document.getElementById("game-score");
+
+let score = 0;
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+cards.forEach((card) => card.addEventListener("click", flipCard));
+
+function flipCard() {
+  console.log(this);
+  this.classList.add("flip");
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
+
+  secondCard = this;
+  hasFlippedCard = false;
+  checkForMatch();
+}
